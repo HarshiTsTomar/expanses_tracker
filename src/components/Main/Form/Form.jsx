3b302e8fbe3e1ced429d955a9132a3d3c711f51a
Harshit <harshitt59@gmail.com>
2023-01-28 21:57:47 +0530

    added context

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
+import useStyles from './styles';
+import { useState } from 'react';
+
+const initialState = {
+  amount: '',
+  category: '',
+  type: 'Income',
+  date: new Date(),
+ }
 
 const Form = () => {
+  const classes = useStyles(); 
+  const [formData, setformData] = useState(initialState);
+
   return (
-    <div>
-      
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography align="center" variant='subtitle2' gutterBottom>
+
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
+            <MenuItem value="Income">Income</MenuItem>
+            <MenuItem value="Expense">Expense</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Category</InputLabel>
+          <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
+            <MenuItem value="business">Business</MenuItem>
+            <MenuItem value="salary">Salary</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+         <Textfield type="number" label="Amount" value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} fullWidth /> 
+      </Grid>
+      <Grid item xs={6}>
+         <Textfield type="date" label="Date" value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} /> 
+      </Grid>
+      <Button className={classes.button} variant="outlined" color="primary" fullWidth >Create</Button>
+    </Grid>
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Card, CardHeader,CardContent, Typography, Grid, Divider } from'@material-ui/core';
+import { Card, CardHeader,CardContent, Typography, Grid, Divider, List } from'@material-ui/core';
 
 import useStyles from './styles';
 import { CallMissedSharp } from '@material-ui/icons';
+import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
   const classes = useStyles();
@@ -16,12 +18,12 @@ const Main = () => {
            {/* <InfoCard /> */}
          </Typography>
          <Divider />
-         {/* <Form /> */}
+         <Form />
         </CardContent>
         <CardContent className={classes.cartContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List /> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
